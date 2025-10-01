2ac799a00c43ec40b87451dccab3fedfef6eaf43
ThanhCongHoang <congblade92@gmail.com>
2025-10-01 18:56:19 +0700

    18:56 01-10-2025

--- src/main/java/com/example/pharmacymanagementsystem_qlht/controller/ChiTietHoaDon_Ctrl.java
+++ src/main/java/com/example/pharmacymanagementsystem_qlht/controller/ChiTietHoaDon_Ctrl.java
@@ -9,9 +9,9 @@ import javafx.stage.Stage;
 public class ChiTietHoaDon_Ctrl extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/com/example/pharmacymanagementsystem_qlht/CN_QuanLy/CN_QLHoaDon/ChiTietHoaDon_GUI.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/com/example/pharmacymanagementsystem_qlht/ChiTietHoaDon_GUI.fxml"));
         Scene scene = new Scene(root);
-        scene.getStylesheets().add(getClass().getResource("").toExternalForm());
+
         stage.setScene(scene);
         stage.show();
     }

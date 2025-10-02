32ca882302310470badb1fac8032bf0017361898
haobodoi <hao.dep.dzai3105@gmail.com>
2025-10-02 11:04:11 +0700

    commmit

--- src/main/java/com/example/pharmacymanagementsystem_qlht/controller/ThongKe/ThongKe_GUI.java
+++ src/main/java/com/example/pharmacymanagementsystem_qlht/controller/ThongKe/ThongKe_GUI.java
@@ -27,14 +27,11 @@ public class ThongKe_GUI extends Application {
         ThongKeBanHang_Ctrl thongkeBanHang_ctrl = new ThongKeBanHang_Ctrl();
         loader.setController(thongkeBanHang_ctrl);
         Pane root = loader.load();
-<<<<<<< Updated upstream
         Scene scene = new Scene(root, 1200, 704);
         scene.getStylesheets().add(
                 getClass().getResource("/com/example/pharmacymanagementsystem_qlht/css/ThongKeBanHang.css").toExternalForm()
         );
-=======
         Scene scene = new Scene(root, 1200, 750);
->>>>>>> Stashed changes
         primaryStage.setTitle("ThongKe");
         primaryStage.setScene(scene);
         primaryStage.show();
@@ -43,6 +40,25 @@ public class ThongKe_GUI extends Application {
         // managed sẽ ràng buộc theo visible
         chartDoanhThu.managedProperty().bind(chartDoanhThu.visibleProperty());
         tableDoanhThu.managedProperty().bind(tableDoanhThu.visibleProperty());
+        cboMQT.getItems().addAll(
+                "Lợi nhuận",
+                "Bán hàng",
+                "Nhân viên",
+                "Khách hàng"
+
+        );
+        cboThoiGian.getItems().addAll(
+                "Hôm nay",
+                "Tuần này",
+                "Tháng này",
+                "Quý này"
+
+        );
+        cboXuatfile.getItems().addAll(
+                "Excel",
+                "PDF"
+
+        );
     }
     private void hienThiBieuDo(ActionEvent event) {
         chartDoanhThu.setVisible(true);

4e9f7ddaf7a5222eb9b5b596b7454f5f908d4dfd
Hiep <dophuhiep212@gmail.com>
2025-10-02 10:13:23 +0700

    Revert "9h35 210"
    This reverts commit 2981c914cf61cfd5bf671d612ce368de8f972862.

---
+++ src/main/java/com/example/pharmacymanagementsystem_qlht/controller/LapHoaDon_Ctrl.java
@@ -0,0 +1,20 @@
+package com.example.pharmacymanagementsystem_qlht.controller;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class LapHoaDon_Ctrl extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/pharmacymanagementsystem_qlht/CN_BanHang/CN_LapHoaDon/LapHoaDon_GUI.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+}

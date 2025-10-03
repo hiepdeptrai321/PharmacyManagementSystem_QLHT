01958f0202afd9f7a295eda70d4664c40b49c299
haobodoi <hao.dep.dzai3105@gmail.com>
2025-10-04 00:50:12 +0700

    commit

--- src/main/java/com/example/pharmacymanagementsystem_qlht/model/PhieuDoiHang.java
+++ src/main/java/com/example/pharmacymanagementsystem_qlht/model/PhieuDoiHang.java
@@ -1,13 +1,106 @@
 package com.example.pharmacymanagementsystem_qlht.model;
 
 import java.sql.Timestamp;
+import java.util.Objects;
 
 public class PhieuDoiHang {
     private String maPD;
-    private NhanVien maNV;
-    private KhachHang maKH;
+    private NhanVien nhanVien;
+    private KhachHang khachHang;
     private Timestamp ngayLap;
     private String lyDoDoi;
     private String ghiChu;
-    private HoaDon maHD;
+    private HoaDon hoaDon;
+
+
+    public PhieuDoiHang(String maPD, NhanVien nhanVien, KhachHang khachHang, Timestamp ngayLap, String lyDoDoi, String ghiChu, HoaDon hoaDon) {
+        this.maPD = maPD;
+        this.nhanVien = nhanVien;
+        this.khachHang = khachHang;
+        this.ngayLap = ngayLap;
+        this.lyDoDoi = lyDoDoi;
+        this.ghiChu = ghiChu;
+        this.hoaDon = hoaDon;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        PhieuDoiHang that = (PhieuDoiHang) o;
+        return Objects.equals(maPD, that.maPD);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(maPD);
+    }
+
+    public String getMaPD() {
+        return maPD;
+    }
+
+    public void setMaPD(String maPD) {
+        this.maPD = maPD;
+    }
+
+    public NhanVien getNhanVien() {
+        return nhanVien;
+    }
+
+    public void setNhanVien(NhanVien nhanVien) {
+        this.nhanVien = nhanVien;
+    }
+
+    public KhachHang getKhachHang() {
+        return khachHang;
+    }
+
+    public void setKhachHang(KhachHang khachHang) {
+        this.khachHang = khachHang;
+    }
+
+    public Timestamp getNgayLap() {
+        return ngayLap;
+    }
+
+    public void setNgayLap(Timestamp ngayLap) {
+        this.ngayLap = ngayLap;
+    }
+
+    public String getLyDoDoi() {
+        return lyDoDoi;
+    }
+
+    public void setLyDoDoi(String lyDoDoi) {
+        this.lyDoDoi = lyDoDoi;
+    }
+
+    public String getGhiChu() {
+        return ghiChu;
+    }
+
+    public void setGhiChu(String ghiChu) {
+        this.ghiChu = ghiChu;
+    }
+
+    public HoaDon getHoaDon() {
+        return hoaDon;
+    }
+
+    public void setHoaDon(HoaDon hoaDon) {
+        this.hoaDon = hoaDon;
+    }
+
+    @Override
+    public String toString() {
+        return "PhieuDoiHang{" +
+                "maPD='" + maPD + '\'' +
+                ", nhanVien=" + nhanVien +
+                ", khachHang=" + khachHang +
+                ", ngayLap=" + ngayLap +
+                ", lyDoDoi='" + lyDoDoi + '\'' +
+                ", ghiChu='" + ghiChu + '\'' +
+                ", hoaDon=" + hoaDon +
+                '}';
+    }
 }
--- src/main/java/com/example/pharmacymanagementsystem_qlht/model/Thuoc_SanPham.java
+++ src/main/java/com/example/pharmacymanagementsystem_qlht/model/Thuoc_SanPham.java
@@ -1,5 +1,7 @@
 package com.example.pharmacymanagementsystem_qlht.model;
 
+import java.util.Objects;
+
 public class Thuoc_SanPham {
     private String maThuoc;
     private String tenThuoc;
@@ -14,4 +16,126 @@ public class Thuoc_SanPham {
 //    private String maLH;
     private String hinhAnh;
 //    private String vitri;
+
+
+    public Thuoc_SanPham(String maThuoc, String tenThuoc, int hamLuong, String donViHamLuong, String duongDung, String quyCachDongGoi, String SDK_GPNK, String hangSX, String nuocSX, String hinhAnh) {
+        this.maThuoc = maThuoc;
+        this.tenThuoc = tenThuoc;
+        this.hamLuong = hamLuong;
+        this.donViHamLuong = donViHamLuong;
+        this.duongDung = duongDung;
+        this.quyCachDongGoi = quyCachDongGoi;
+        this.SDK_GPNK = SDK_GPNK;
+        this.hangSX = hangSX;
+        this.nuocSX = nuocSX;
+        this.hinhAnh = hinhAnh;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Thuoc_SanPham that = (Thuoc_SanPham) o;
+        return Objects.equals(maThuoc, that.maThuoc);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(maThuoc);
+    }
+
+    public String getMaThuoc() {
+        return maThuoc;
+    }
+
+    public void setMaThuoc(String maThuoc) {
+        this.maThuoc = maThuoc;
+    }
+
+    public String getTenThuoc() {
+        return tenThuoc;
+    }
+
+    public void setTenThuoc(String tenThuoc) {
+        this.tenThuoc = tenThuoc;
+    }
+
+    public int getHamLuong() {
+        return hamLuong;
+    }
+
+    public void setHamLuong(int hamLuong) {
+        this.hamLuong = hamLuong;
+    }
+
+    public String getDonViHamLuong() {
+        return donViHamLuong;
+    }
+
+    public void setDonViHamLuong(String donViHamLuong) {
+        this.donViHamLuong = donViHamLuong;
+    }
+
+    public String getDuongDung() {
+        return duongDung;
+    }
+
+    public void setDuongDung(String duongDung) {
+        this.duongDung = duongDung;
+    }
+
+    public String getQuyCachDongGoi() {
+        return quyCachDongGoi;
+    }
+
+    public void setQuyCachDongGoi(String quyCachDongGoi) {
+        this.quyCachDongGoi = quyCachDongGoi;
+    }
+
+    public String getSDK_GPNK() {
+        return SDK_GPNK;
+    }
+
+    public void setSDK_GPNK(String SDK_GPNK) {
+        this.SDK_GPNK = SDK_GPNK;
+    }
+
+    public String getHangSX() {
+        return hangSX;
+    }
+
+    public void setHangSX(String hangSX) {
+        this.hangSX = hangSX;
+    }
+
+    public String getNuocSX() {
+        return nuocSX;
+    }
+
+    public void setNuocSX(String nuocSX) {
+        this.nuocSX = nuocSX;
+    }
+
+    public String getHinhAnh() {
+        return hinhAnh;
+    }
+
+    public void setHinhAnh(String hinhAnh) {
+        this.hinhAnh = hinhAnh;
+    }
+
+    @Override
+    public String toString() {
+        return "Thuoc_SanPham{" +
+                "maThuoc='" + maThuoc + '\'' +
+                ", tenThuoc='" + tenThuoc + '\'' +
+                ", hamLuong=" + hamLuong +
+                ", donViHamLuong='" + donViHamLuong + '\'' +
+                ", duongDung='" + duongDung + '\'' +
+                ", quyCachDongGoi='" + quyCachDongGoi + '\'' +
+                ", SDK_GPNK='" + SDK_GPNK + '\'' +
+                ", hangSX='" + hangSX + '\'' +
+                ", nuocSX='" + nuocSX + '\'' +
+                ", hinhAnh='" + hinhAnh + '\'' +
+                '}';
+    }
 }

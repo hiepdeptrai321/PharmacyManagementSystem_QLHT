f09f9fc5c82bc1dcf7613d0afeb5234540d0a2cd
haobodoi <hao.dep.dzai3105@gmail.com>
2025-10-05 14:46:56 +0700

    commit

---
+++ src/main/java/com/example/pharmacymanagementsystem_qlht/controller/CN_QuanLy/QLNhanVien/SuaXoaNhanVien.java
@@ -0,0 +1,16 @@
+package com.example.pharmacymanagementsystem_qlht.controller.CN_QuanLy.QLNhanVien;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class SuaXoaNhanVien extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}

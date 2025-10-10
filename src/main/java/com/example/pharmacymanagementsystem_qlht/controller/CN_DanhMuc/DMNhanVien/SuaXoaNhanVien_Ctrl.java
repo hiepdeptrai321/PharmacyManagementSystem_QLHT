cd8f64537b53781ad4d693fb49ba1696f7d3198c
hiepdeptrai321 <23683411.hiep@student.iuh.edu.vn>
2025-10-10 17:17:47 +0700

    5:17 1010

---
+++ src/main/java/com/example/pharmacymanagementsystem_qlht/controller/CN_DanhMuc/DMNhanVien/SuaXoaNhanVien_Ctrl.java
@@ -0,0 +1,22 @@
+package com.example.pharmacymanagementsystem_qlht.controller.CN_DanhMuc.DMNhanVien;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class SuaXoaNhanVien_Ctrl extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/com/example/pharmacymanagementsystem_qlht/CN_DanhMuc/DMNhanVien/SuaXoaNhanVien_GUI.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}

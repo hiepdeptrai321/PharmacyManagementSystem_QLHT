29a17568a34d1264eb9515853ab1ecfbcf208129
haobodoi <hao.dep.dzai3105@gmail.com>
2025-10-27 18:58:03 +0700

    commit

--- src/main/java/com/example/pharmacymanagementsystem_qlht/controller/CuaSoChinh_QuanLy_Ctrl.java
+++ src/main/java/com/example/pharmacymanagementsystem_qlht/controller/CuaSoChinh_QuanLy_Ctrl.java
@@ -87,6 +87,7 @@ public class CuaSoChinh_QuanLy_Ctrl{
             e.printStackTrace();
             System.err.println("Không thể tải FXML: " + fxmlPath);
         }
+        pnlChung.requestFocus();
     }
 
     public void loadTableThuocHetHan(){
